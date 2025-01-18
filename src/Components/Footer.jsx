3e3e98ca9b2b2b21b39fbb2a6872bec2232fcaf0
Koushik Mehta <koushikmehta07@gmail.com>
2025-01-18 11:32:17 +0530

    Update Footer.jsx
    edit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,50 @@
-import React from 'react';
-import './Footer.css';
+
+import React from "react";
+import { Link } from 'react-router-dom';
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>
-        © 2025 <span className="code-icon">{"</>"}</span> Developed by Koushik Mehta
-      </p>
+      <div className="footer-container">
+        <div className="footer-section">
+          <h3 className="footer-title">Quick Links</h3>
+          <ul className="footer-links">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/skills">Skills</Link></li>
+            <li><Link to="/certifications">Certifications</Link></li>
+            <li> <Link to="/myproject">My Project</Link></li>
+            <li> <Link to="/contact">Contact</Link></li>
+          </ul>
+        </div>
+        <div className="footer-section follow-me-section">
+          <h3>Follow Me</h3>
+          <div className="social-icons">
+        <a href="https://www.linkedin.com/in/koushik-mehta-7a4975285/" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a href="https://github.com/KoushikMehta31" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-instagram"></i>
+        </a>
+      </div>
+        </div>
+        <div className="footer-section">
+          <h3>Contact Us</h3>
+          <p>Jharkhand, India</p>
+          <p>Jamshedpur</p>
+          <p>Email: koushikmehta07@gmail.com</p>
+        </div>
+      </div>
+      <hr className="line"/>
+      <div className="footer-bottom">
+        <p>© 2025 {"</>"} Developed by Koushik Mehta</p>
+      </div>
     </footer>
   );
 };
 
 export default Footer;
+
